6538385b219cab049bcfb10838867e23eb51752a
Nick Villapiano <nvillapiano@gmail.com>
2018-04-05 11:35:40 -0400

    Responsive considerations for content width and layout

--- dist/1844e3ae2563291095589f3ed04790de.js
+++ dist/1844e3ae2563291095589f3ed04790de.js
@@ -138,7 +138,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":12}],31:[function(require,module,exports) {
+},{"_css_loader":12}],34:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52925' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49821' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -261,5 +261,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[31])
+},{}]},{},[34])
 //# sourceMappingURL=/dist/1844e3ae2563291095589f3ed04790de.map
